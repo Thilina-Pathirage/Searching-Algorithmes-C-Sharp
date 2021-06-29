7b0d640c2bb118fa58b002d13a177fb92921c244
Thilina-Pathirage <thilinapathirage@gmail.com>
2021-06-29 06:19:43 +0530

    binarySearch

---
+++ binarySearchAlgo/binarySearchAlgo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace binarySearchAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array = { 3, 4, 5, 6, 7, 8, 9 };
+            int n = array.Length;
+            int x = 5;
+
+            binarySearch b = new binarySearch();
+            int result = b.binarySearchmethod(array, x, 0, n - 1);
+            if (result == -1)
+                Console.WriteLine("Not Found!");
+            else
+                Console.WriteLine("Element is found at index " + result);
+        }
+    }
+}
---
+++ binarySearchAlgo/binarySearchAlgo/binarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace binarySearchAlgo
+{
+    class binarySearch
+    {
+        public int binarySearchmethod(int[] items, int sk, int low, int high)
+        {
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (items[mid] == sk)
+                    return mid;
+
+                if (items[mid] < sk)
+                    low = mid + 1;
+
+                else
+                    high = mid - 1;
+            }
+
+            return -1;
+        }
+    }
+}
